07f9499a61479e028a202c96a402f864f2fb11e5
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-04-03 17:46:04 +0700

    Create Ex4.py

---
+++ Luyện tập 14/Ex4.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+    def get_name(self):
+        return self.__name
+
+    def get_price(self):
+        return self.__price
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_price(self, price):
+        self.__price = price
+
+book1 = Book("Python", 30000)
+
+print("Giá sách:", book1.get_price())
