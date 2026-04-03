40ce81c1ca7644900bbb8f23df5afd17160a58be
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-04-03 17:18:15 +0700

    Create Ex2.py

---
+++ Luyện tập 14/Ex2.py
@@ -0,0 +1,7 @@
+names = []
+for i in range(5):
+    name = input(f"Nhập tên người thứ {i+1}: ")
+    names.append(name)
+print("Danh sách ban đầu:", names)
+del names[1]
+print("Danh sách sau khi xóa:", names)
