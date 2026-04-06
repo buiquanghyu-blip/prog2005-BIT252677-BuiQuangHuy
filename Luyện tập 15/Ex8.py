ffb74669da3a37aa6de60a078bbddd6972258d97
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-04-06 17:31:52 +0700

    Create Ex8.py

---
+++ Luyện tập 15/Ex8.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(self, price):
+        self.set_price(price)
+
+    def get_price(self):
+        return self._price
+
+    def set_price(self, price):
+        if price < 0:
+            print("Lỗi: giá không hợp lệ!")
+        else:
+            self._price = price
+p = Product(100)
+print(p.get_price())
+
+p.set_price(-50)  # lỗi
